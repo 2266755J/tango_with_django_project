448934588d41d4c5ac90fbe8e3490c489565a075
Olga Jodelka <2266755J@student.gla.ac.uk>
2018-01-26 10:52:58 +0000

    exercises chapter 4 and 5 done

--- populate_rango.py
+++ populate_rango.py
@@ -29,13 +29,13 @@ def populate():
         {"title":"Flask",
          "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views" :128, "likes" :64 },
+            "Django": {"pages": django_pages, "views" : 64, "likes" : 32},
+            "Other Frameworks": {"pages": other_pages, "views" :32, "likes" :16} }
 
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -54,12 +54,10 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
-    v = Category.objects.get_or_create(views=views)[0]
-    l = Category.objects.get_or_create(likes=likes)[0]
+    c.views = views
+    c.likes = likes
     c.save()
-    v.save()
-    l.save()
-    return c, v, l
+    return c
 
 if __name__ == '__main__':
     print("Starting Rango population script...")
