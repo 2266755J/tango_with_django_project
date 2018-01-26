fd2077526aade55d1fe48ed4bb07b471ae9a4d41
Olga Jodelka <2266755J@student.gla.ac.uk>
2018-01-26 13:29:05 +0000

    update category table with a slug field

--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories' : category_list}
-    return render(request, 'rango/index.html', context = context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_dict = {'message' : "Rango with his friend"}
