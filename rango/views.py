2d7a4b152bb792a050ba1cb4a09b9006573698c0
Olga Jodelka <2266755J@student.gla.ac.uk>
2018-01-26 13:53:18 +0000

    chapter 6 done

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 from django.http import HttpResponse
 
@@ -11,3 +12,18 @@ def index(request):
 def about(request):
     context_dict = {'message' : "Rango with his friend"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', conetext_dict)
